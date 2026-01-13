169f7c364fb400aff1f7a3b97e7fc7f4cf361193
ttttom.1 <chanhotom@gmail.com>
2026-01-13 09:41:02 +0900

    feat: 슬라이딩 북 스타일 UI 및 백틱 에러 수정

--- src/main/java/hello/hello_spring/repository/ChatNodeRepository.java
+++ src/main/java/hello/hello_spring/repository/ChatNodeRepository.java
@@ -1,9 +1,15 @@
 package hello.hello_spring.domain.chat; // 방법 B라면 패키지 경로를 repository로 수정!
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.lang.reflect.Field;
 import java.util.List;
 
 public interface ChatNodeRepository extends JpaRepository<ChatNode, Long> {
     // 특정 사용자의 모든 대화 내용을 시간순으로 가져오는 기능
     List<ChatNode> findByMemberIdOrderByCreatedAtAsc(Long memberId);
+
+    List<ChatNode> findByParentIdOrderByCreatedAtAsc(Long parentId);
+
+    List<ChatNode> findTop3ByMemberIdAndFieldOrderByCreatedAtDesc(Long memberId, DomainField field);
 }
